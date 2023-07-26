f7b6b32e7ababec7c21d8e0f86728f6e6dea12ac
John Godday <talk2john40@gmail.com>
2023-07-26 19:36:48 +0100

    Added Task 4 solution

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number > 0 and int(str(number)[-1]) > 5:
     print('Last digit of {} is {} and is greater than 5'.format(str(number), str(number)[-1]))
-elif number > 0 and  int(str(number)[-1]) == 0:
+elif number >= 0 and  int(str(number)[-1]) == 0:
     print('Last digit of {} is {} and is 0'.format(str(number), str(number)[-1]))
 else:
     print('Last digit of {} is -{} and is less than 6 and not 0'.format(str(number), str(number)[-1]))
