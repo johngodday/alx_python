e1fb4a8f99c47818b0f334a78073eb51680b65e1
John Godday <talk2john40@gmail.com>
2023-07-28 13:04:29 +0100

    Added solution to function task

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,14 @@
+#!/user/bin/pyhton3
+def fibonacci_sequence(n):
+    val = [0, 1]
+    if n == 0:
+        return []
+    elif n == 1:
+        return [0]
+    else:
+         while len(val) < n:
+             cal = val[-1] + val[-2]
+             val.append(cal)
+    return val
+
+print(fibonacci_sequence(20))
