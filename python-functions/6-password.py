285e3b9307b8c5f1079806e9ec45ab6c810cee6a
John Godday <talk2john40@gmail.com>
2023-08-09 20:18:09 +0100

    Added task solution

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,3 +1,24 @@
 #!user/bin/python3
-def validate_password():
-    pass
+def validate_password(password):
+    if len(password) <  8:
+        return True
+    
+    upper_check = False
+    lower_check = False
+    digit_check = False
+
+    for i in password:
+        if i.isupper():
+            upper_check = True
+        elif i.islower():
+            lower_check = True
+        elif i.isdigit():
+            digit_check = True
+
+    if not(upper_check and lower_check and digit_check):
+        return False
+    
+    if " " in password:
+        return False
+    
+    return True
