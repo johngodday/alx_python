8ae6ec731d27cc547fa8b4ef62a0ecb3baf12d46
John Godday <talk2john40@gmail.com>
2023-08-10 02:53:01 +0100

    updated prime file

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,10 +1,16 @@
 #!/user/bin/python3
-def is_prime(n):
-    if n > 1:
-        for i in range(2,  int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-            else:
-                return True
-    else:
+def is_prime(number):
+    if number <= 1:
         return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+print(is_prime(15))
