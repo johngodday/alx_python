8e4f085d64390003715fdbd426204b9996305742
John Godday <talk2john40@gmail.com>
2023-08-19 15:46:55 +0100

    Added solution to task

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,7 +1,2 @@
 def common_elements(set_1, set_2):
-    return set(set_1.intersection(set_2))
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-
-print(common_elements(set_1, set_2))
+    return set_1.intersection(set_2)
