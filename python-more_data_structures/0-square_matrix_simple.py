fe1c724c363bd9b95ee882891dc1aede8a5b798b
John Godday <talk2john40@gmail.com>
2023-08-19 15:37:54 +0100

    Added solution to task

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
 def square_matrix_simple(matrix=[]):
     emp_ls = []
-    for x in ls:
+    for x in matrix:
         rs = [i ** 2 for i in x]
         emp_ls.append(rs)
     return emp_ls
-
-ls = [[2,3,4], [5,6,8]]
-    
-print(square_matrix_simple(ls))
