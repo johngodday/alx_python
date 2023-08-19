12b2f03fee13c779383ab258744970af71317499
John Godday <talk2john40@gmail.com>
2023-08-19 17:33:12 +0100

    added solution to task

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,9 +1,12 @@
 def best_score(a_dictionary):
 
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
     ls_score = [score for name, score in a_dictionary.items()]
     best_key = ''
-
     for name, score in a_dictionary.items():
         if score == max(ls_score):
             best_key += name
+
     return best_key            
