77b71c332f5f0b69461885b5fa36d0285cfa2890
John Godday <talk2john40@gmail.com>
2023-08-19 11:25:35 +0100

    added task solutions

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,4 +1,7 @@
 def multiple_returns(sentence):
     len_of_string = len(sentence)
-    first_char = sentence[0]
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
     return len_of_string, first_char
