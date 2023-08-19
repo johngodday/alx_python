04f94812240015d4ad8ff523c6efce4fe5513786
John Godday <talk2john40@gmail.com>
2023-08-19 11:30:51 +0100

    added task solutions

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,8 +2,9 @@ def print_matrix_integer(matrix=[[]]):
     for x in matrix:
           for y in range(len(x)):
             print('{:d}'.format(x[y]), end = "")
-            if y < len(x) - 1:
+            if y != len(x) - 1:
                 print(" ", end = "")
-    print()
+
+          print()
 
 
